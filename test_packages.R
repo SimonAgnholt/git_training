c298091e3c520af8b11cafb5fa769417cf477d33
[madssojberg] <[madssojberg@gmail.com]>
2025-09-22 09:30:08 +0000

    SLAP NU AF

--- test_packages.R
+++ test_packages.R
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-git branch -M main 
 library(tidyverse)
 library(DBI)
 library(RPostgres) #Hello World
-test
-=======
-git branch -M main
-library(tidyverse)
-library(DBI)
-library(RPostgres) # Hello World
 
 # VÆRSGO UNGE MAND. FUCK NU DE HVIIIIEEEE!!!!
->>>>>>> c95bfb551b0e68df5398f759082dd15520fc478d
