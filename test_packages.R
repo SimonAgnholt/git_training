9f052856f71c044bddbd7d122328ee0ff0f9e7de
[madssojberg] <[madssojberg@gmail.com]>
2025-11-19 14:50:52 +0000

    LOL

--- test_packages.R
+++ test_packages.R
@@ -1,5 +1,16 @@
-library(tidyverse)
-library(DBI)
-library(RPostgres) #Hello World
+###############################################
+# 🚨 SCIENTIFICALLY VERIFIED R WARNING 🚨
+#
+# You have discovered the *Forbidden Chunk of Code™*.
+# Legend says anyone who reads this comment
+# gains +3 statistical power and -1 sanity.
+#
+# Please proceed with:
+#   plot(chaos ~ caffeine)
+# and remember:
+#   if (p < 0.05) celebrate responsibly.
+#
+# This message will self-destruct
+# as soon as someone finally learns ggplot2.
+###############################################
 
-# VÆRSGO UNGE MAND. FUCK NU DE HVIIIIEEEE!!!!
